ad8dc76e2a3cf34a1dfc2a3feae49def14d0cde6
Launay <launay.ronan@gmail.com>
2014-07-28 10:26:05 +0200

    Update MacPorts.bash

--- MacPorts.bash
+++ MacPorts.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-usermod -a -G admin $USER
+usermod -G admin $USER
 wget https://distfiles.macports.org/MacPorts/MacPorts-2.3.1-10.9-Mavericks.pkg
 installer -allowUntrusted -verboseR -pkg MacPorts-2.3.1-10.9-Mavericks.pkg
