331883b549727b8521f910d40593915267a442dc
ronanlaunay <launay.ronan@gmail.com>
2019-01-05 17:21:39 +0100

    feat(vbox_fuse_switch.sh): adding vbox fuse switcher for using macos fuse and vbox on macos high sierra

--- MacPorts.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-xcode-select --install
-usermod -G admin $USER
-wget https://distfiles.macports.org/MacPorts/MacPorts-2.3.1-10.9-Mavericks.pkg
-installer -allowUntrusted -verboseR -pkg MacPorts-2.3.1-10.9-Mavericks.pkg
-port -v selfupdate
