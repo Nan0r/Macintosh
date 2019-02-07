eb16f69f0aa9292496f2254c5e49aa8eacc94d2f
ronanlaunay <launay.ronan@gmail.com>
2019-02-07 15:34:15 +0100

    feat(macos): adding new shell script for kext, disk and permissions

---
+++ volume_repair.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $(id -u) == 0 ]; then
+  echo "You need to run this script as standard user, not root (pid may be 501). Type id -u in terminal to check this"
+exit
+fi
+diskutil repairVolume /dev/disk0
+echo "Finished"
